9e4984f94a7cf2dc801980ab1fdd48ae10f7ab2f
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-12-02 23:12:17 +0100

    modal gg

--- js/post.js
+++ js/post.js
@@ -34,13 +34,19 @@ async function getPost() {
       data._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
 
     content.innerHTML += `
-      <div onclick="myFunction()" class="postContent">${data.content.rendered}</div>`;
+      <div class="postContent">${data.content.rendered}</div>`;
 
     const author = document.querySelector(".wp-block-post-author__name");
     author.innerHTML = `<h3>Author: ${data._embedded["author"][0].name}</h3>`;
 
-    modal.innerHTML = `<div onclick="close()" class="esc">&times;</div><img onclick="close()" class="postImage" src="${imagePost}" alt=" post image"> `;
-
+    // modal.innerHTML = `<div class="esc">&times;</div><img class="postImage" src="${imagePost}" alt=" post image"> `;
+    document.querySelectorAll("img").forEach((img) => {
+      img.addEventListener("click", (e) => {
+        document.querySelector(".modal").classList.add("active");
+        document.querySelector("#lightbox-figure img").src = e.target.src;
+        document.querySelector("#lightbox-caption").innerHTML = e.target.alt;
+      });
+    });
     console.log("Success");
     const loader = document.querySelector(".loader");
     loader.classList.add("hide");
@@ -53,11 +59,20 @@ async function getPost() {
 }
 getPost();
 
+// document.querySelectorAll("img").forEach((img) => {
+//   img.addEventListener("click", (e) => {
+//     document.querySelector(".lightbox").classList.add("active");
+//     document.querySelector("#lightbox-figure img").src = e.target.src;
+//     document.querySelector("#lightbox-caption").innerHTML = e.target.alt;
+//   });
+// });
+
 function myFunction() {
-  console.log("yo");
-  document.querySelector(".modal").classList.add("show");
+  console.log("yo check check");
+  document.querySelector(".active").classList.remove("active");
 }
-function close() {
-  console.log("yo2");
-  document.querySelector(".modal").classList.remove("show");
-}
+
+// function close() {
+//   console.log("yo2");
+//   document.querySelector(".modal").classList.remove("show");
+// }
