b4cc0852cf3cf5f03917515a5588585b70545b7e
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2022-12-02 17:29:06 +0100

    posts

--- js/posts.js
+++ js/posts.js
@@ -1,7 +1,15 @@
 SSbreadcrumb.innerHTML += `<li><a href="posts.html">All News Updates</a></li>`;
 pageTitle.innerHTML += `All News Updates`;
 
+const viewMore = document.querySelector(".viewMore");
+const loader = document.querySelector(".loader");
+
+// 10 posts
 const url = "https://flowpow.pro/glitch/wp-json/wp/v2/posts?_embed";
+
+// more posts
+const url2 =
+  "https://flowpow.pro/glitch/wp-json/wp/v2/posts?_embed&per_page=100";
 
 async function fetchPosts() {
   try {
@@ -11,34 +19,67 @@ async function fetchPosts() {
     console.log(json);
     const posts = json;
 
+    content.innerHTML = "";
+
     posts.forEach(function (post) {
       const titlePost = post.title.rendered;
       const imagePost =
-        post._embedded["wp:featuredmedia"][0].media_details.sizes.medium
+        post._embedded["wp:featuredmedia"][0].media_details.sizes.full
           .source_url;
+
       content.innerHTML += `
       <div class="postItem">
-      <div class="postImage"> 
- <img src="${imagePost}" alt="${imagePost.alt_text}" <h3>${titlePost}"
-         </h3>     </img>  </div>
+      <a href="post.html?id=${post.id}" class="postLink">
+
+ <img class="postImage" src="${imagePost}" alt=" post image"> 
         <h3>${titlePost}"
-         </h3>   
+         </h3>  
+         </a> 
       </div>`;
     });
     console.log("Success");
-    const loader = document.querySelector(".loader");
-    loader.classList.remove("loader");
+    loader.classList.add("hide");
   } catch (error) {
     // catch
     console.log(error);
     content.innerHTML = "An error occured";
   }
+}
+fetchPosts();
 
-  // Add more buttom
-  content.innerHTML += ` <div class="postItem itemButton">
-                        <button class="button viewMore">View All Posts</button>
+// fetch more than 10 posts
+async function fetchPosts2() {
+  try {
+    const response = await fetch(url2);
+    const json = await response.json();
 
-                </div>`;
+    console.log(json);
+    const posts = json;
+
+    content.innerHTML = "";
+
+    posts.forEach(function (post) {
+      const titlePost = post.title.rendered;
+      const imagePost =
+        post._embedded["wp:featuredmedia"][0].media_details.sizes.full
+          .source_url;
+      content.innerHTML += `
+      <div class="postItem">
+      <a href="post.html?id=${post.id}" class="postLink">
+
+ <img class="postImage" src="${imagePost}" alt=" post image"> 
+        <h3>${titlePost}"
+         </h3>  
+         </a> 
+      </div>`;
+    });
+    console.log("Success");
+    viewMore.classList.add("hide");
+  } catch (error) {
+    // catch
+    console.log(error);
+    content.innerHTML = "An error occured";
+  }
 }
 
-fetchPosts();
+viewMore.addEventListener("click", fetchPosts2);
